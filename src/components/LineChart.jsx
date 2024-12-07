48df59d577348b4d6ad55e38dc50265a918da6bf
Leo McClellan <mcclellan2@usf.edu>
2024-12-07 14:25:34 -0500

    Complete Line Chart

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,24 +1,26 @@
 import ChartComponent from "./Chart";
 
 export const LineChart = ({ data }) => {
-    const lineChartData = {
-      labels: data.months,
+  // Functional components to render a line chart  
+  const lineChartData = {
+      labels: data.months, // x-axis labels based on months from the data prop
       datasets: [
         {
           label: 'Monthly Sales',
-          data: data.sales,
-          backgroundColor: 'rgba(75, 192, 192, 0.2)',
+          data: data.sales, // y-axis values based on the sales from the data prop
+          backgroundColor: 'rgba(75, 192, 192, 0.2)', // styles the chart's lines
           borderColor: 'rgba(75, 192, 192, 1)',
           borderWidth: 1,
         },
       ],
     };
     const lineChartOptions = {
-        scales: {
+        scales: { //configures the chart axes
           y: {
-            beginAtZero: true,
+            beginAtZero: false, // fits the y axis to the graph
           },
         },
       };
+    // passes type, data, and options props to ChartComponent to render the line chart
     return <ChartComponent type="line" data={lineChartData} options={lineChartOptions} />;
 };
--- src/components/ScatterChart.jsx
+++ src/components/ScatterChart.jsx
@@ -1,29 +1,26 @@
 import ChartComponent from "./Chart";
 
 export const ScatterChart = ({ data }) => {
-    const scatterChartData = {
-      labels: data.months,
+  // Functional components to render a scatter chart  
+  const scatterChartData = {
+      labels: data.months, // x-axis labels based on months from the data prop
       datasets: [
         {
           label: 'Monthly Sales',
-          data: data.sales,
-          backgroundColor: 'rgba(75, 192, 192, 0.2)',
+          data: data.sales, // y-axis values based on the sales from the data prop
+          backgroundColor: 'rgba(75, 192, 192, 0.2)', // styles the chart
           borderColor: 'rgba(75, 192, 192, 1)',
           borderWidth: 1,
         },
       ],
     };
     const scatterChartOptions = {
-        type: 'scatter',
-        data: data,
-        options: {
-            scales: {
-                x: {
-                    type: 'linear',
-                    position: 'bottom'
-                }
-            }
-        }
-    };
+        scales: { //configures the chart axes
+          y: {
+            beginAtZero: true, // ensures the Y-axis from zero for a consistent scale
+          },
+        },
+      };
+    // passes type, data, and options props to ChartComponent to render the scatter chart
     return <ChartComponent type="scatter" data={scatterChartData} options={scatterChartOptions} />;
 };
