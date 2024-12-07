be2ba87ebdd464b1c8c03d8ff241fee1a1ef2e81
Leo McClellan <mcclellan2@usf.edu>
2024-12-07 12:39:19 -0500

    Innitialize React environment with Chart.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import React, { useEffect, useRef } from 'react';
+import { BarChart } from './BarChart';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [chartData, setChartData] = useState(null);
+  useEffect(() => {
+    fetch('/financial_data.json')
+      .then((response) => response.json())
+      .then((data) => setChartData(data));
+  }, []);
 
+  if (!chartData) {
+    return <div>Loading...</div>;
+  }
+  
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div style={{ textAlign: 'center' }}>
+        <h1>Dynamic Charts with React and Chart.js</h1>
+        <BarChart data={chartData} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/BarChart.jsx
@@ -0,0 +1,24 @@
+import ChartComponent from "./Chart";
+
+export const BarChart = ({ data }) => {
+    const barChartData = {
+      labels: data.months,
+      datasets: [
+        {
+          label: 'Monthly Sales',
+          data: data.sales,
+          backgroundColor: 'rgba(75, 192, 192, 0.2)',
+          borderColor: 'rgba(75, 192, 192, 1)',
+          borderWidth: 1,
+        },
+      ],
+    };
+    const barChartOptions = {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      };
+    return <ChartComponent type="bar" data={barChartData} options={barChartOptions} />;
+};
